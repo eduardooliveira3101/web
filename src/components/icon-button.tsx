33a2c9dc9e1f4a35f6b70b873f1d76901a32cb68
Carlos Eduardo <c.edu31@hotmail.com>
2025-02-18 21:30:37 -0300

    stage 01 finally

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,13 +1,12 @@
-import { ReactNode } from "react"
+import { type ComponentProps, ReactNode } from "react";
 
-interface ButtonProps {
- children: ReactNode
-}
+interface ButtonProps extends ComponentProps<"button"> {}
 
-export function IconButton (props: ButtonProps) {
-  return (
-    <button className="px-1.5 bg-gray-500  text-blue rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-400">
-      {props.children}
-    </button>
-  )
+export function IconButton(props: ButtonProps) {
+	return (
+		<button
+			className="px-1.5 bg-gray-500  text-blue rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-400"
+			{...props}
+		/>
+	);
 }
